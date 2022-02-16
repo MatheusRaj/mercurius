61a6c36b8c46562b84005e67f6d97f02c39b3197
MatheusRaj <matheusraj5@gmail.com>
2022-02-16 17:02:24 -0300

    change server to async

--- src/io.ts
+++ src/io.ts
@@ -3,11 +3,15 @@ import { Server } from 'socket.io';
 
 import app from './app';
 
-export const server = new http.Server(app);
+export const httpServer = new http.Server(app);
 
-export const initServer = (port: number) => server.listen(port);
+export const initServer = async (port: number) => {
+  const server = await httpServer.listen({ port });
 
-export const getSocket = (): Server => {
+  return server;
+};
+
+export const getSocket = (server: http.Server): Server => {
   const io = new Server(server);
 
   return io;
