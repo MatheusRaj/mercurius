0ad3bd444a076d767e162423810b90ba05aa4261
MatheusRaj <matheusraj5@gmail.com>
2022-02-16 13:36:37 -0300

    fix index file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'source-map-support/register';
 
 import * as sentry from '@sentry/node';
 
-import './io';
-import './socket';
+export * from './io';
+export * from './socket';
 
 export const initSentry = (sentryKey: string) => sentry.init({ dsn: sentryKey });
