bc61835ad424a212eb69459d70f7656b2a01166f
MatheusRaj <matheusraj5@gmail.com>
2022-03-03 15:42:53 -0300

    remove mongo

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 import * as sentry from '@sentry/node';
 import { Connection } from '@eduzz/rabbit';
-import { mongoConnect } from './mongo';
 import { BehaviorSubject } from 'rxjs';
 import { httpServer } from './io';
 
@@ -9,13 +8,11 @@ const rabbitConnection = new BehaviorSubject<Connection>({} as Connection);
 export const getRabbitConnection = () => rabbitConnection.getValue();
 
 export const config = async (params: any) => {
-  const { sentryKey, rabbitParams, mongoParams, port } = params;
+  const { sentryKey, rabbitParams, port } = params;
 
   sentry.init({ dsn: sentryKey });
 
   rabbitConnection.next(new Connection(rabbitParams));
-
-  await mongoConnect(mongoParams);
 
   await httpServer.listen({ port });
   console.log('Server up and listening port: ', port);
