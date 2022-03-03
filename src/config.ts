20378c78d348ac692f6b95156b0c851beb29021a
MatheusRaj <matheusraj5@gmail.com>
2022-03-03 15:52:45 -0300

    add mongo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import * as sentry from '@sentry/node';
 import { Connection } from '@eduzz/rabbit';
+import { mongoConnect } from './mongo';
 import { BehaviorSubject } from 'rxjs';
 import { httpServer } from './io';
 
@@ -8,11 +9,13 @@ const rabbitConnection = new BehaviorSubject<Connection>({} as Connection);
 export const getRabbitConnection = () => rabbitConnection.getValue();
 
 export const config = async (params: any) => {
-  const { sentryKey, rabbitParams, port } = params;
+  const { sentryKey, rabbitParams, mongoParams, port } = params;
 
   sentry.init({ dsn: sentryKey });
 
   rabbitConnection.next(new Connection(rabbitParams));
+
+  await mongoConnect(mongoParams);
 
   await httpServer.listen({ port });
   console.log('Server up and listening port: ', port);
