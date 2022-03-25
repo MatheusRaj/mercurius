30dc1bf65a18916b05893df2d88a4021cc74ecba
MatheusRaj <matheusraj5@gmail.com>
2022-03-25 14:01:14 -0300

    change http route to root

--- src/dev.ts
+++ src/dev.ts
@@ -2,16 +2,15 @@ import * as http from 'http';
 
 import express from 'express';
 
-import { config, router } from '.';
+import { router } from '.';
 
 async function init() {
   const app = express();
   const httpServer = new http.Server(app);
   const port = 3001;
-  await config({ port, httpServer });
 
-  app.use('/mercurius', router);
-  httpServer.listen(3001, () => console.log('Server up and listening port: ', port));
+  app.use('/', router);
+  httpServer.listen(3001, () => console.log('Server dev up and listening port: ', port));
 }
 
 init();
